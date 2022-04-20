c949a7f30079ec06246da8ba12f1ab17396d4864
Maddison Hellstrom <maddy@na.ai>
2022-04-19 19:13:10 -0700

    Fix: 'extend' transform should handle list-like tables

--- lua/incline/config/transform.lua
+++ lua/incline/config/transform.lua
@@ -1,7 +1,13 @@
 local M = {}
 
 M.extend = function(val, entry)
-  return vim.tbl_extend('force', entry.default, val)
+  if vim.tbl_islist(entry.default) and vim.tbl_islist(val) then
+    local res = vim.deepcopy(entry.default)
+    vim.list_extend(res, val)
+    return res
+  else
+    return vim.tbl_extend('force', entry.default, val)
+  end
 end
 
 M.replace = function(val)
